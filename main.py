5446cbf8ab355c0be1e27035522b598ba35a71f1
miathemunchkin <mia.rokosowski@gmail.com>
2024-06-09 16:34:36 +0300

    fixes

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ HTTP_PORT = 3000
 SOCKET_PORT = 5000
 DATA_FILE = "storage/data.json"
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 app.static_folder = STATIC_FOLDER
 app.template_folder = TEMPLATE_FOLDER
